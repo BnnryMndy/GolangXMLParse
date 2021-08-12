9b69bcef49ea28301e719d5fa863114bdea0e723
BnnryMndy <bnnrymndy@gmail.com>
2021-08-12 14:44:21 +0300

    project restructurisation and add gin

---
+++ server.go
@@ -0,0 +1,27 @@
+package xmlparse
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpserver *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpserver = &http.Server{
+		Addr:           ":" + port,
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout:    10 + time.Second,
+		WriteTimeout:   10 + time.Second,
+	}
+
+	return s.httpserver.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpserver.Shutdown(ctx)
+}
